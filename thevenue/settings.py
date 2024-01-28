856808a6de5a1b47b451b1d50dc3f4aef18dc054
felipesimao1 <felipe.simao1@gmail.com>
2024-01-28 02:23:12 +0000

    fixing heroku

--- thevenue/settings.py
+++ thevenue/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'thevenue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'thevenue_db',
         'USER': 'postgres',
         'PASSWORD': 'admin',
